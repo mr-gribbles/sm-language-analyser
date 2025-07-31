d3465673e6737dc9ffb10d5f277f4f37a3169189
Logan Barrington <loganbarrington25@gmail.com>
2025-07-31 13:15:43 +1200

    feat: Consolidate requirements; remove lightweight requirements file and update build script for dependency installation

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Railway build script
 
-echo "Installing lightweight dependencies..."
-pip install -r requirements-railway.txt
+echo "Installing dependencies..."
+pip install -r requirements.txt
 
 echo "Downloading NLTK data..."
 python -c "import nltk; nltk.download('vader_lexicon', quiet=True); nltk.download('punkt', quiet=True)"
